d392a5293ff2f9dc92692ddcc13440f61ab76682
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-10-10 21:15:20 +0200

    Improving build scropt.

--- build.sh
+++ build.sh
@@ -6,18 +6,28 @@ giturl=https://github.com/substance/substance.git
 remoteurl=git@github.com:substance/docs.git
 source=/tmp/substance
 build=/tmp/substance/dist/doc
+docs=/tmp/docs
 
 # Check to see if repo exists. If not, git clone it
 if [ ! -d $source ]; then
-  echo "Cloning repository..."
+  echo "Cloning substance/substance..."
   git clone $giturl $source
+fi
+if [ ! -d $docs ]; then
+  echo "Cloning substance/docs..."
+  git clone $remoteurl $docs
 fi
 
 # Git checkout appropriate branch, pull latest code
+echo "Updating repositories..."
 cd $source
-echo "Updating repository..."
 git checkout $branch
 git fetch origin $branch
+git reset --hard FETCH_HEAD
+cd -
+cd $docs
+git checkout gh-pages
+git fetch origin gh-pages
 git reset --hard FETCH_HEAD
 cd -
 
@@ -28,16 +38,18 @@ npm install
 echo "Executing 'npm run doc'..."
 npm run doc
 message=$(git log -1 --pretty=%B)
+echo "Updating docs..."
+rsync -r --delete --cvs-exclude /tmp/substance/dist/doc/ /tmp/docs/
 cd -
 
-cd "$build"
-if git diff-index --quiet HEAD --; then
-  echo "Documentation did not change."
-else
+cd "$docs"
+if git status --porcelain; then
   echo "Pushing changes into 'substance/docs'..."
   git add .
   git commit -m "$message"
   git push origin gh-pages
+else
+  echo "Documentation did not change."
 fi
 cd -
 
