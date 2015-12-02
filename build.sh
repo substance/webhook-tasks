3e953cb68ec8295911ac4b86e2c8beebbaf52327
Daniel Beilinson <beilinson@gmail.com>
2015-12-02 21:38:38 +0300

    don't touch index

--- build.sh
+++ build.sh
@@ -39,8 +39,8 @@ echo "Executing 'npm run doc'..."
 npm run doc
 message=$(git log -1 --pretty=%B)
 echo "Updating docs..."
-# Place assets contents to /docs
-rsync -r --delete --cvs-exclude /app/assets/ /tmp/docs/
+# Uncomment to place assets contents to /docs
+# rsync -r --delete --cvs-exclude /app/assets/ /tmp/docs/
 # Sync master branch documentation with latest release 
 rsync -r --delete --cvs-exclude /tmp/substance/dist/ /tmp/docs/master
 cd -
